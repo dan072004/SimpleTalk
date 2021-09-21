5e96bae6a3ac9f3590bfeaebaf63351cd3fa93fe
Daniel Moldovan <dan072004@yahoo.com>
2021-09-21 17:17:37 +0300

    first commit

---
+++ ST-KafkaProducer/ST-KafkaProducer/Controllers/KafkaProducerController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+
+namespace ST_KafkaProducer.Controllers
+{
+	[Route("api/kafka")]
+	[ApiController]
+	public class KafkaProducerController : ControllerBase
+	{
+		private readonly ProducerConfig config = new ProducerConfig
+			{ BootstrapServers = "localhost:9092" };
+		private readonly string topic = "simpletalk_topic";
+		[HttpPost]
+		public IActionResult Post([FromQuery] string message)
+		{
+			var result = Created(string.Empty, SendToKafka(topic, message));
+			return result;
+		}
+
+		private Object SendToKafka(string topic, string message)
+		{
+			using (var producer =
+				new ProducerBuilder<Null, string>(config).Build())
+			{
+				try
+				{
+					return producer.ProduceAsync(topic, new Message<Null, string> { Value = message })
+						.GetAwaiter()
+						.GetResult();
+				}
+				catch (Exception e)
+				{
+					Console.WriteLine($"Oops, something went wrong: {e}");
+				}
+			}
+			return null;
+		}
+	}
+}
